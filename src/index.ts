340570dec1e4f4d144e9068dea712d4513d219e8
benibana2001 <benibana2001@gmail.com>
2019-10-29 09:34:54 +0900

    a sligth edit

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import Stories from './Stories'
-let stories = new Stories(50)
+let stories = new Stories(10)
 stories.getData().then(
     () => {
         console.log(stories.stories)
     }
 )
 
+let e: Element = document.body
+let eDiv: Element = document.createElement('div')
+eDiv.textContent = "Hello World"
+e.appendChild(eDiv)
