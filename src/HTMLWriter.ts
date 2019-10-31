50f8baa16a047dc9eb2ff7d3608dd2c4f0439e1f
benibana2001 <benibana2001@gmail.com>
2019-10-31 19:14:19 +0900

    edit css

--- src/HTMLWriter.ts
+++ src/HTMLWriter.ts
@@ -17,47 +17,26 @@ export default class HTMLWriter implements Writer {
         **      - a.Anchor Area
         **          - .rank
         **          - .title
-        **          - .score
+        **          - a.score
+        **              - span
         ****************************j
         */
 
-        // debug
         let card: DOMCreator = new DOMCreator('div', parent, 'card', null, null)
         if (typeof sd.url === 'string' && typeof sd.title === 'string' && typeof sd.score === 'number') {
-            let anchorArea: DOMCreator = new DOMCreator('a', card.elem, null, sd.url, null)
+            let anchorArea: DOMCreator = new DOMCreator('a', card.elem, 'anchorArea', sd.url, null)
             let rank: DOMCreator = new DOMCreator('div', anchorArea.elem, 'rank', null, String(999))
             let title: DOMCreator = new DOMCreator('div', anchorArea.elem, 'title', null, sd.title)
-            let score: DOMCreator = new DOMCreator('div', card.elem, 'score', null, String(sd.score))
+            let score: DOMCreator = new DOMCreator('a', card.elem, 'score', null, null)
+            let scoreSpan: DOMCreator = new DOMCreator('span', score.elem, 'scoreSpan', null, String(sd.score))
 
             rank.add()
             title.add()
             anchorArea.add()
+            scoreSpan.add()
             score.add()
             card.add()
         }
-        // **************************
-
-        /*
-        // Create Card
-        let card: Element = document.createElement('div')
-        card.className = "card"
-
-        // Create AnchorArea
-        let anchorArea: Element = document.createElement('a')
-        if (typeof sd.url === 'string') anchorArea.setAttribute("href", sd.url)
-        // if (typeof sd.url === 'string') this.createAnchor(card, null, sd.url)
-        // Add Rank
-        this.createDIV(anchorArea, 'rank', "999")
-        // Add Title
-        if (typeof sd.title === 'string') this.createDIV(anchorArea, 'title', sd.title)
-        // Add Score
-        if (typeof sd.score === 'number') this.createDIV(anchorArea, 'score', String(sd.score))
-
-        // Append AnchorArea
-        card.appendChild(anchorArea)
-        // Append Card
-        parent.appendChild(card)
-        */
     }
 
     private createDIV(parent: Element, className: string | null, text: string | null): void {
@@ -66,11 +45,4 @@ export default class HTMLWriter implements Writer {
         if (text !== null) div.textContent = text
         parent.appendChild(div)
     }
-    /*
-        private createAnchor(parent: Element, className: string | null, url: string | null): void {
-            let a: Element = document.createElement('a')
-            if (className !== null) a.className = className
-            if (url !== null) a.setAttribute("href", url)
-        }
-    */
 }
