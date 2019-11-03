a88a464a957563735a2cbf611d2d7da5a432b830
benibana2001 <benibana2001@gmail.com>
2019-11-03 20:51:31 +0900

    rename some function

--- src/index.ts
+++ src/index.ts
@@ -9,17 +9,18 @@ import HTMLWriter from './HTMLWriter'
 let stryCollector = new StoryCollecter(30)
 let iterator: StoriesIterator = stryCollector.iterator()
 let hw = new HTMLWriter()
-let stryAry: StoryData[] = []// StoryDataを複数格納する
+let stryPacket: StoryData[] = []// StoryDataを指定個数だけ格納する, 上書きされる
 
 // fetchをキュー
 let queueNxtStry = async () => {
     // Rawデータを格納
     let sd: StoryData = await iterator.next()
-    stryAry.push(sd)
+    stryPacket.push(sd)
 }
 
 let quereAry: Promise<any>[] = []// queueのPromiseを格納
-let view = async (): Promise<any> => {
+// StoryData取得・描画の基準点
+let initLoading = async (): Promise<any> => {
     await stryCollector.init()
     while (iterator.hasNext()) {
         quereAry.push(queueNxtStry())
@@ -27,35 +28,49 @@ let view = async (): Promise<any> => {
 
     // スクロールローディングのLOCK開始
     lockLoadTrigger()
-    sort()
+    render()
 }
 
-let sort = async (): Promise<any> => {
+// SotryDataの全パケット受信完了を待機
+let doneFetchPacket = async (): Promise<any> => {
     await Promise.all(quereAry)
-    // console.log("sort実行")
-    let sortedStryAry: StoryData[] = iterator.sortAryBbl(stryAry)
+}
+
+let render = async (): Promise<any> => {
+    await doneFetchPacket()
+
+    // 並び替え実行
+    let sortedStryAry: StoryData[] = iterator.sortAryBbl(stryPacket)
     console.log(sortedStryAry)
+    // 全件書き出し
     for (let i = 0; i < sortedStryAry.length; i++) {
         console.log(`${i} を書き込み中( been writing ${i})`)
         await hw.write(sortedStryAry[i])
     }
     console.log("書き込み完了 ( finished writing )")
-    // 一度配列を空にする
-    stryAry = []
+    // パケットを空にする
+    stryPacket = []
     // ロックを解除する
     UnLockLoadTrigger()
 }
 
-view()
+initLoading()
 
 // 通信、Card書き込み処理中はロック
 let ticking: boolean = false
+let lockLoadTrigger = (): void => {
+    ticking = true
+}
+let UnLockLoadTrigger = (): void => {
+    ticking = false
+}
+
 window.addEventListener('scroll', () => {
     let lastScrllY: number = window.scrollY
     if (!ticking) {
         window.requestAnimationFrame(() => {
             // console.log(isOnPageBttm())
-            if (isOnPageBttm()) view()
+            if (isOnPageBttm()) initLoading()
             ticking = false
         })
         ticking = true
@@ -66,10 +81,3 @@ window.addEventListener('scroll', () => {
 
 // TODO: 最下部への到達が早いとスクロールイベントをキャッチできない
 // よってOnPageBttm の状態が数秒間続いた時は view() を実行
-
-let lockLoadTrigger = (): void => {
-    ticking = true
-}
-let UnLockLoadTrigger = (): void => {
-    ticking = false
-}
