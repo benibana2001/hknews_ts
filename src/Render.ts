53ec3b41eb85c19952609fa75193812db0ef369d
benibana2001 <benibana2001@gmail.com>
2019-11-04 00:54:37 +0900

    divide render function from index.ts

---
+++ src/Render.ts
@@ -0,0 +1,84 @@
+import StoryCollecter from './ts/Story/StoryCollecter'
+import StoriesIterator from './ts/Story/StoriesIterator'
+import { StoryData } from './ts/HKNews'
+import { isOnPageBttm } from './ts/Utility'
+import HTMLWriter from './ts/Writer/HTMLWriter'
+
+export default class Render {
+    public stryCollector: StoryCollecter = new StoryCollecter(30)
+    public iterator: StoriesIterator = this.stryCollector.iterator()
+    public hw = new HTMLWriter()
+    public stryPacket: StoryData[] = []// StoryDataを指定個数だけ格納する, 上書きされる
+    public quereAry: Promise<any>[] = []// queueのPromiseを格納
+    public ticking: boolean = false
+
+    constructor() {
+        window.addEventListener('scroll', () => {
+            let lastScrllY: number = window.scrollY
+            if (!this.ticking) {
+                window.requestAnimationFrame(() => {
+                    // console.log(isOnPageBttm())
+                    if (isOnPageBttm()) this.initLoading()
+                    this.ticking = false
+                })
+                this.ticking = true
+            } else {
+                console.log("ページ下部に到達。書き込み完了を待機. ( YOU CAME BOTOOM OF THE PAGE, BUT PLEASE WAIT SO THAT WRITING WILL FINISH SAFELY )")
+            }
+        })
+    }
+
+    public lockLoadTrigger = (): void => {
+        this.ticking = true
+    }
+
+    public UnLockLoadTrigger = (): void => {
+        this.ticking = false
+    }
+
+    public isLocked = (): boolean => {
+        return this.ticking
+    }
+
+    // fetchをキュー
+    public queueNxtStry = async (): Promise<any> => {
+        // Rawデータを格納
+        let sd: StoryData = await this.iterator.next()
+        this.stryPacket.push(sd)
+    }
+    // StoryData取得・描画の基準点
+    public initLoading = async (): Promise<any> => {
+        await this.stryCollector.init()
+        while (this.iterator.hasNext()) {
+            this.quereAry.push(this.queueNxtStry())
+        }
+
+        // スクロールローディングのLOCK開始
+        this.lockLoadTrigger()
+        this.render()
+    }
+    // SotryDataの全パケット受信完了を待機
+    public doneFetchPacket = async (): Promise<any> => {
+        await Promise.all(this.quereAry)
+    }
+    
+    // 通信、Card書き込み処理中はロック
+    public render = async (): Promise<any> => {
+        await this.doneFetchPacket()
+
+        // 並び替え実行
+        let sortedStryAry: StoryData[] = this.iterator.sortAryBbl(this.stryPacket)
+        console.log(sortedStryAry)
+        // 全件書き出し
+        for (let i = 0; i < sortedStryAry.length; i++) {
+            console.log(`${i} を書き込み中( been writing ${i})`)
+            await this.hw.write(sortedStryAry[i])
+        }
+        console.log("書き込み完了 ( finished writing )")
+        // パケットを空にする
+        this.stryPacket = []
+        // ロックを解除する
+        this.UnLockLoadTrigger()
+    }
+
+}
--- src/index.ts
+++ src/index.ts
@@ -1,83 +1,24 @@
 import './scss/style.scss'
-
-import StoryCollecter from './ts/Story/StoryCollecter'
-import StoriesIterator from './ts/Story/StoriesIterator'
-import { StoryData } from './ts/HKNews'
+import Render from './Render'
 import { isOnPageBttm } from './ts/Utility'
-import HTMLWriter from './ts/Writer/HTMLWriter'
 
-let stryCollector = new StoryCollecter(30)
-let iterator: StoriesIterator = stryCollector.iterator()
-let hw = new HTMLWriter()
-let stryPacket: StoryData[] = []// StoryDataを指定個数だけ格納する, 上書きされる
+const renderer: Render = new Render()
 
-// fetchをキュー
-let queueNxtStry = async () => {
-    // Rawデータを格納
-    let sd: StoryData = await iterator.next()
-    stryPacket.push(sd)
-}
+// window.addEventListener('scroll', () => {
+//     if (!renderer.isLocked) {
+//         window.requestAnimationFrame(() => {
+//             if (isOnPageBttm) renderer.initLoading()
+//             console.log("UN-LOCK")
+//             renderer.UnLockLoadTrigger()
+//         })
+//         console.log("LOCK")
+//         renderer.lockLoadTrigger()
+//     } else {
+//         // console.log("ページ下部に到達。書き込み完了を待機. ( YOU CAME BOTOOM OF THE PAGE, BUT PLEASE WAIT SO THAT WRITING WILL FINISH SAFELY )")
+//     }
+// })
 
-let quereAry: Promise<any>[] = []// queueのPromiseを格納
-// StoryData取得・描画の基準点
-let initLoading = async (): Promise<any> => {
-    await stryCollector.init()
-    while (iterator.hasNext()) {
-        quereAry.push(queueNxtStry())
-    }
-
-    // スクロールローディングのLOCK開始
-    lockLoadTrigger()
-    render()
-}
-
-// SotryDataの全パケット受信完了を待機
-let doneFetchPacket = async (): Promise<any> => {
-    await Promise.all(quereAry)
-}
-
-let render = async (): Promise<any> => {
-    await doneFetchPacket()
-
-    // 並び替え実行
-    let sortedStryAry: StoryData[] = iterator.sortAryBbl(stryPacket)
-    console.log(sortedStryAry)
-    // 全件書き出し
-    for (let i = 0; i < sortedStryAry.length; i++) {
-        console.log(`${i} を書き込み中( been writing ${i})`)
-        await hw.write(sortedStryAry[i])
-    }
-    console.log("書き込み完了 ( finished writing )")
-    // パケットを空にする
-    stryPacket = []
-    // ロックを解除する
-    UnLockLoadTrigger()
-}
-
-initLoading()
-
-// 通信、Card書き込み処理中はロック
-let ticking: boolean = false
-let lockLoadTrigger = (): void => {
-    ticking = true
-}
-let UnLockLoadTrigger = (): void => {
-    ticking = false
-}
-
-window.addEventListener('scroll', () => {
-    let lastScrllY: number = window.scrollY
-    if (!ticking) {
-        window.requestAnimationFrame(() => {
-            // console.log(isOnPageBttm())
-            if (isOnPageBttm()) initLoading()
-            ticking = false
-        })
-        ticking = true
-    } else {
-        console.log("ページ下部に到達。書き込み完了を待機. ( YOU CAME BOTOOM OF THE PAGE, BUT PLEASE WAIT SO THAT WRITING WILL FINISH SAFELY )")
-    }
-})
+renderer.initLoading()
 
 // TODO: 最下部への到達が早いとスクロールイベントをキャッチできない
 // よってOnPageBttm の状態が数秒間続いた時は view() を実行
